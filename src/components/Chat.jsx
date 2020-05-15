01791546b2f0c139c73ead68bae0f6ba6ef37cea
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-15 19:43:34 +0900

    回答を表示するメソッドを作成

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,11 +7,11 @@ import Neko from '../assets/img/torahack.png'
 
 const Chat = (props) => {
   const isQuestion = (props.type ==='question');
-  const classes = isQuestion ? 'p-chat__reverse' : 'p-chat__reverse';
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
 
   return(
-    <ListItem>
-      <ListItemAvatar className={classes}>
+    <ListItem className={classes}>
+      <ListItemAvatar >
         {isQuestion ? (
           <Avatar alt="icon" src={Neko} />
         ) : (
