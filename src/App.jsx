cca67f00f16a99ce93920a8bb39d071c0e0cb931
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-16 18:45:59 +0900

    お問い合わせボタンを押したら表示させる

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css'
-import {AnswersList, Chats} from "./components/index"
+import {AnswersList, Chats, FormDialog} from "./components/index"
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +14,8 @@ export default class App extends React.Component {
         open:false
     };
     this.selectAnswer = this.selectAnswer.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -36,12 +38,18 @@ export default class App extends React.Component {
       case (nextQuestionId === 'init'):
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
+
+      case (nextQuestionId === 'contact'):
+        this.handleClickOpen();
+        break;
+
       case (/^https:*/.test(nextQuestionId)):
         const a = document.createElement('a');
         a.href = nextQuestionId;
         a.target ='_blank';
         a.click();
         break;
+
       default:
         const chats = this.state.chats;
         chats.push({
@@ -56,6 +64,14 @@ export default class App extends React.Component {
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000);
         break;
     }
+  };
+
+  handleClickOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
   };
 
   // initAnswer = () => {
@@ -101,6 +117,7 @@ export default class App extends React.Component {
         <div className="c-box">
           <Chats chats={this.state.chats} />
           <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
+          <FormDialog open={this.state.open} handleClose={this.handleClose} />
         </div>
       </section>
     );
