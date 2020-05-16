9f6b5825a1b70baf068fadc48933c0c4f2892bc7
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-16 17:31:26 +0900

    componentDidUpdateの作成

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,13 @@ export default class App extends React.Component {
     // this.initAnswer();
   }
 
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area')
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight
+    }
+  }
+
   render() {
     return (
       <section className="c-section">
